da1cbdb799ca26d106f54c9870e05ed293986594
Colin Silvers <silversc1998@gmail.com>
2023-07-02 18:44:48 -0400

    component progress

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useState, useEffect} from 'react';
+import Workout from "./Workout";
+
 
 const Dashboard = () => {
+
+    const [workoutList, setWorkoutList] = useState([]);
+
+
+
     return (
         <div>
             <p>This is the dashboard.</p>
+            {workoutList}
         </div>
     )
 };
