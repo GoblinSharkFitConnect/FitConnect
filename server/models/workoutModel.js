082be6c8d76d8e927a241f0bc6f7887b8438f7c6
Tim Weidinger <timothy.weidinger@gmail.com>
2023-07-03 08:03:05 -0400

    monday morning commit

--- server/models/workoutModel.js
+++ server/models/workoutModel.js
@@ -43,19 +43,20 @@ class Workout {
   }
   // updating the workout in the DB
   static async updateWorkout(id, name, goal, day) {
-    if (!id) {
-      console.error('Error trying to verify session, no ssid');
-      return null;
-    }
-    // // execute a table join on members and SSID to return the member information
-    const query = {
-      text: `
-        SELECT m.* FROM member m
-        LEFT OUTER JOIN session s ON s.user_id = ug.id
-        WHERE s.ssid = $1;
-      `,
-      values: [ssid],
-    };
+    // need to figure out how the form submission works here
+    // if (!id) {
+    //   console.error('Error trying to verify session, no ssid');
+    //   return null;
+    // }
+    // // // execute a table join on members and SSID to return the member information
+    // const query = {
+    //   text: `
+    //     SELECT m.* FROM member m
+    //     LEFT OUTER JOIN session s ON s.user_id = ug.id
+    //     WHERE s.ssid = $1;
+    //   `,
+    //   values: [ssid],
+    // };
     // try {
     //   const response = await db.query(query);
     //   if (!response.rows[0]) {
