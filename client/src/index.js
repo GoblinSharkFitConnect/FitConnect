034fd75beb47fbfc240599916bbd30993cd8f106
Joie Zhang <joie@princeton.edu>
2023-07-02 20:04:42 -0700

    tried to add a BrowserRouter to NavBar.jsx

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client'
-import App from "./App.jsx"
-import { BrowserRouter as Router } from 'react-router-dom'
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <Router>
-    <App />
-  </Router>
-);
-
 // import React from 'react';
-// import { BrowserRouter } from 'react-router-dom';
-// import { render } from 'react-dom';
-// import App from './App.jsx';
+// import { createRoot } from 'react-dom/client'
+// import App from "./App.jsx"
+// import { BrowserRouter as Router } from 'react-router-dom'
 
-// render(
-//   <BrowserRouter>
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(
+//   <Router>
 //     <App />
-//   </BrowserRouter>,
-//   document.getElementById('root'),
+//   </Router>
 // );
+
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render } from 'react-dom';
+import App from './App.jsx';
+
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
